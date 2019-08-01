664c9839d5452df548261e7c9a13934c26124915
Michael Fudge <mafudge@gmail.com>
2019-08-01 11:26:50 -0400

    Website published 08/01/2019 11:26:40

---
+++ js/readthedocs.js
@@ -0,0 +1,7 @@
+$("input[name='q']")
+  .attr( "placeholder","Search Site" );
+$("div.wy-side-nav-search>a")
+  .attr("class","")
+  .html("<img class='logo' src='/images/logo.png' />");
+$("ul.wy-breadcrumbs>li>a")
+  .html("IST256");
